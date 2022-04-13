205c050518a463d408da0a20a5c589e904c3c569
danielledoesburg <daniellemd@gmail.com>
2022-04-13 11:54:23 +0200

    first controller test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,13 +2,29 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class HomeController
 {
     #[Route('/')]
-    public function homepage()
+    public function homepage(): Response
     {
-        die('testing');
+        return new Response('dit is de homepage');
     }
+
+    #[Route('/recipes/{slug}')]
+    public function recipe(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'recipe: '.u(str_replace('-', ' ', $slug))->title();
+        } else {
+            $title = 'All recipes';
+        }
+
+        return new Response($title);
+    }
+
+
 }
